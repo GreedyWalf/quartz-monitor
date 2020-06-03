3bc8e8b5199710ef1473796d3c192928da41b78f
GreedyWalf <695830237@qq.com>
2020-06-03 18:21:15 +0800

    使用freemarker视图；

--- src/main/java/com/qs/quartz/controller/IndexController.java
+++ src/main/java/com/qs/quartz/controller/IndexController.java
@@ -1,9 +1,13 @@
 package com.qs.quartz.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-
+    @RequestMapping(value = {"/", "/index"})
+    public String index() {
+        return "/index";
+    }
 }
---
+++ src/main/webapp/template/index.ftl
@@ -0,0 +1,10 @@
+<#assign ctx=Request.contextPath/>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>登录页面</title>
+</head>
+<body>
+this is index.ftl
+</body>
+</html>
